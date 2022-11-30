caa0776db5142f5f275895dec7558646bcf802b6
Deepak Malhotra <deepak.malhotra@crowdbotics.com>
2022-11-30 16:30:13 +0530

    test

--- backend/modules/urls.py
+++ backend/modules/urls.py
@@ -6,7 +6,6 @@ from django.db.utils import ProgrammingError
 
 urlpatterns = []
 
-
 # BE CAREFUL! Do not remove or change this code snippet, this is needed to get
 # Crowdbotics' official modules working properly.
 
